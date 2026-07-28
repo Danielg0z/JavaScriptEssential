98346da9d05036c8459cbab467853a9c8262669e
Danielg0z <danielokonkwo.po@gmail.com>
2026-07-28 12:33:16 -0500

    Finished the Practice function

--- jscoursetesting/CalculateArea/calculate_Area.js
+++ jscoursetesting/CalculateArea/calculate_Area.js
@@ -13,3 +13,22 @@ function calculateArea(length, width) {
     return area;
 }
 
+let grocery1;
+let grocery2;
+let grocery3;
+
+function calculateTotal(grocery1, grocery2, grocery3) {
+    // the document.getElementById retrieves the user input from the HTML input field with the id 'grocery1' and converts it to a floating-point number using parseFloat.
+    grocery1 = parseFloat(document.getElementById("grocery1").value);
+    // the document.getElementById retrieves the user input from the HTML input field with the id 'grocery2' and converts it to a floating-point number using parseFloat.
+    grocery2 = parseFloat(document.getElementById("grocery2").value);
+    // the document.getElementById retrieves the user input from the HTML input field with the id 'grocery3' and converts it to a floating-point number using parseFloat.
+    grocery3 = parseFloat(document.getElementById("grocery3").value);
+
+    let total = grocery1 + grocery2 + grocery3;
+    document.getElementById('total').innerText = `The total cost of groceries is : ${total}`;
+    return total;
+}
+
+
+
